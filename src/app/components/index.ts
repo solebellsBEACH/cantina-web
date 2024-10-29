b20a297481c24f18badbe72ca446b5237972c5b3
solebellsBEACH <lucassxxavier@gmail.com>
2024-10-29 14:19:41 -0300

    feat: improving structure

---
+++ src/app/components/index.ts
@@ -0,0 +1,7 @@
+import { CommonComponents } from "./common";
+import { HomeComponents } from "./home";
+
+export const LibComponents = {
+    HomeComponents,
+    CommonComponents
+}
---
+++ src/app/product/page.tsx
@@ -0,0 +1,3 @@
+export default function Product() {
+    return <h1>Teste</h1>
+}
